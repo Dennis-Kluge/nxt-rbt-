915e931de0d584e0d545043337bc53e67cd4cb49
Swen Hutop <Swen@Swens-MacBook-Pro.local>
2012-09-24 21:48:25 +0200

    Klasse für Dijkstra Algorithmus hinzugefügt.

---
+++ src/nxt/rbt/graph/DijkstraAlgorithm.java
@@ -0,0 +1,133 @@
+package nxt.rbt.graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class DijkstraAlgorithm {
+
+	private final List<Node> nodes;
+	  private final List<Edge> edges;
+	  private Set<Node> settledNodes;
+	  private Set<Node> unSettledNodes;
+	  private Map<Node, Node> predecessors;
+	  private Map<Node, Integer> distance;
+
+	  public DijkstraAlgorithm(Graph graph) {
+	    // Create a copy of the array so that we can operate on this array
+	    this.nodes = new ArrayList<Node>(graph.getVertexes());
+	    this.edges = new ArrayList<Edge>(graph.getEdges());
+	  }
+
+	  public void execute(Node source) {
+	    settledNodes = new HashSet<Node>();
+	    unSettledNodes = new HashSet<Node>();
+	    distance = new HashMap<Node, Integer>();
+	    predecessors = new HashMap<Node, Node>();
+	    distance.put(source, 0);
+	    unSettledNodes.add(source);
+	    while (unSettledNodes.size() > 0) {
+	    	Node node = getMinimum(unSettledNodes);
+	      settledNodes.add(node);
+	      unSettledNodes.remove(node);
+	      findMinimalDistances(node);
+	    }
+	  }
+
+	  private void findMinimalDistances(Node node) {
+	    List<Node> adjacentNodes = getNeighbors(node);
+	    for (Node target : adjacentNodes) {
+	      if (getShortestDistance(target) > getShortestDistance(node)
+	          + getDistance(node, target)) {
+	        distance.put(target, getShortestDistance(node)
+	            + getDistance(node, target));
+	        predecessors.put(target, node);
+	        unSettledNodes.add(target);
+	      }
+	    }
+
+	  }
+
+	  private int getDistance(Node node, Node target) {
+	    for (Edge edge : edges) {
+	      if (edge.getSource().equals(node)
+	          && edge.getDestination().equals(target)) {
+	        return (int) edge.getWeight();
+	      }
+	    }
+	    throw new RuntimeException("Should not happen");
+	  }
+
+	  private List<Node> getNeighbors(Node node) {
+	    List<Node> neighbors = new ArrayList<Node>();
+	    for (Edge edge : edges) {
+	      if (edge.getSource().equals(node)
+	          && !isSettled(edge.getDestination())) {
+	        neighbors.add(edge.getDestination());
+	      }
+	    }
+	    return neighbors;
+	  }
+
+	  private Node getMinimum(Set<Node> vertexes) {
+		 Node minimum = null;
+	    for (Node vertex : vertexes) {
+	      if (minimum == null) {
+	        minimum = vertex;
+	      } else {
+	        if (getShortestDistance(vertex) < getShortestDistance(minimum)) {
+	          minimum = vertex;
+	        }
+	      }
+	    }
+	    return minimum;
+	  }
+
+	  private boolean isSettled(Node vertex) {
+	    return settledNodes.contains(vertex);
+	  }
+
+	  private int getShortestDistance(Node destination) {
+	    Integer d = distance.get(destination);
+	    if (d == null) {
+	      return Integer.MAX_VALUE;
+	    } else {
+	      return d;
+	    }
+	  }
+
+	  /*
+	   * This method returns the path from the source to the selected target and
+	   * NULL if no path exists
+	   */
+	  public LinkedList<Node> getPath(Node target) {
+	    LinkedList<Node> path = new LinkedList<Node>();
+	    Node step = target;
+	    // Check if a path exists
+	    if (predecessors.get(step) == null) {
+	      return null;
+	    }
+	    path.add(step);
+	    while (predecessors.get(step) != null) {
+	      step = predecessors.get(step);
+	      path.add(step);
+	    }
+	    // Put it into the correct order
+	    path = reverseList(path);
+	    return path;
+	  }
+
+	
+	  public LinkedList<Node> reverseList(LinkedList<Node> list) {
+		  LinkedList<Node> path = new LinkedList<Node>();
+		  for(int i = 0; i < list.size(); i++) {
+			  path.add(list.get(list.size()-i));
+		  }
+		  return path;
+	  }
+	
+}
